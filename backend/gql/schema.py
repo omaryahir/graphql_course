0459970f28783693d21e780754ffb096a6456c7f
omaryahir <1206997+omaryahir@users.noreply.github.com>
2020-02-10 23:16:34 -0600

    ADD: search support

--- backend/gql/schema.py
+++ backend/gql/schema.py
@@ -2,9 +2,12 @@ from graphene import Argument, Field, ID, ObjectType, Schema
 from graphene_django import DjangoConnectionField
 from notes.models import Note
 from .notes.types import NoteType
+from .notes.filters import NoteFilter
+from graphene_django.filter import DjangoFilterConnectionField
 
 class Query(ObjectType):
-    notes = DjangoConnectionField(NoteType)
+    #notes = DjangoConnectionField(NoteType)
+    notes = DjangoFilterConnectionField(NoteType, filterset_class=NoteFilter)
     note = Field(NoteType, id=Argument(ID, required=True))
 
     def resolve_notes(root, info, **kwargs):
